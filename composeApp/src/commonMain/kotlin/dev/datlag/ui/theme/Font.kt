cda83b7296911f1a9fadfa15cf47087a6779b703
Jeff Retz <jeff.retz08@gmail.com>
2025-06-05 01:35:51 +0200

    lower website and loading size

--- composeApp/src/commonMain/kotlin/dev/datlag/ui/theme/Font.kt
+++ composeApp/src/commonMain/kotlin/dev/datlag/ui/theme/Font.kt
@@ -4,13 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import dev.datlag.composeapp.generated.resources.Manrope_Bold
-import dev.datlag.composeapp.generated.resources.Manrope_ExtraBold
-import dev.datlag.composeapp.generated.resources.Manrope_ExtraLight
-import dev.datlag.composeapp.generated.resources.Manrope_Light
-import dev.datlag.composeapp.generated.resources.Manrope_Medium
-import dev.datlag.composeapp.generated.resources.Manrope_Regular
-import dev.datlag.composeapp.generated.resources.Manrope_SemiBold
 import dev.datlag.composeapp.generated.resources.Manrope_VariableFont_wght
 import dev.datlag.composeapp.generated.resources.Res
 import org.jetbrains.compose.resources.Font
@@ -19,29 +12,26 @@ data object Font {
 
     @Composable
     fun family() = FontFamily(
-        Font(Res.font.Manrope_ExtraLight, FontWeight.ExtraLight),
-        Font(Res.font.Manrope_ExtraLight, FontWeight.ExtraLight, FontStyle.Italic),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.ExtraLight),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.ExtraLight, FontStyle.Italic),
 
-        Font(Res.font.Manrope_Light, FontWeight.Light),
-        Font(Res.font.Manrope_Light, FontWeight.Light, FontStyle.Italic),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.Light),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.Light, FontStyle.Italic),
 
-        Font(Res.font.Manrope_Regular, FontWeight.Normal),
-        Font(Res.font.Manrope_Regular, FontWeight.Normal, FontStyle.Italic),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.Normal),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.Normal, FontStyle.Italic),
 
-        Font(Res.font.Manrope_Medium, FontWeight.Medium),
-        Font(Res.font.Manrope_Medium, FontWeight.Medium, FontStyle.Italic),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.Medium),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.Medium, FontStyle.Italic),
 
-        Font(Res.font.Manrope_SemiBold, FontWeight.SemiBold),
-        Font(Res.font.Manrope_SemiBold, FontWeight.SemiBold, FontStyle.Italic),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.SemiBold),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.SemiBold, FontStyle.Italic),
 
-        Font(Res.font.Manrope_Bold, FontWeight.Bold),
-        Font(Res.font.Manrope_Bold, FontWeight.Bold, FontStyle.Italic),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.Bold),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.Bold, FontStyle.Italic),
 
-        Font(Res.font.Manrope_ExtraBold, FontWeight.ExtraBold),
-        Font(Res.font.Manrope_ExtraBold, FontWeight.ExtraBold, FontStyle.Italic),
-
-        // Fallback
-        Font(Res.font.Manrope_VariableFont_wght),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.ExtraBold),
+        Font(Res.font.Manrope_VariableFont_wght, FontWeight.ExtraBold, FontStyle.Italic),
     )
 
 }
